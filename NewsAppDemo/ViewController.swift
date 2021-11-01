a2bc372c05dbbeadab7d70adee53d1512f38ac64
Rakibur Khan <therakiburkhan@gmail.com>
2021-11-01 18:19:02 +0600

    Initial Commit

---
+++ NewsAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppDemo
+//
+//  Created by Rakibur Khan on 1/Nov/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
